a6d2c914c1a6e7eeffc8a1b180f55167421d936b
ArtemiiLulevich <artemii.lulevich@gmail.com>
2019-04-14 17:38:27 +0300

    Add new code in functions

--- fuctions/functions.py
+++ fuctions/functions.py
@@ -5,15 +5,21 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def center_text(text, sep=' ', end_char='\n', file=None, flush=False):
+def center_text(*args, sep=' ', end_char='\n', file=None, flush=False):
     text = ""
-    for arg in text:
-        text += str(arg) + end_char
+    for arg in args:
+        text += str(arg) + sep
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text, end=end_char, file=file, flush=flush)
+    # print(" " * left_margin, text, end=end_char, file=file, flush=flush)
+    return " " * left_margin + text
 
 
-center_text("sfdgdfgd")
-center_text("spam, spam, eggs and spam")
-center_text(12)
-center_text(1, 2, 5, "print", end_char=' ')
+# with open("centered", mode='w') as centered_file:
+#     center_text("sfdgdfgd", file=centered_file)
+#     center_text("spam, spam, eggs and spam", file=centered_file)
+#     center_text(12, file=centered_file)
+#     center_text(1, 2, 5, "print", file=centered_file)
+print(center_text("sfdgdfgd"))
+print(center_text("spam, spam, eggs and spam"))
+print(center_text(12))
+print(center_text(1, 2, 5, "print"))
