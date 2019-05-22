ae9fa6f573e296f1720f6d235db2c82e075aa8e1
Artemii.Lulevich <artemii.lulevich@gmail.com>
2019-05-22 20:02:23 +0300

    BlackJack game done. Add score table.

--- fuctions/blackJack/blackJackGame.py
+++ fuctions/blackJack/blackJackGame.py
@@ -102,6 +102,44 @@ def deal_dealer():
         result_text.set("Draw!")
 
 
+def start_new_game():
+    global dealer_card_frame
+    global player_card_frame
+    global dealer_hand
+    global player_hand
+    global result_text
+    global deck
+    global cards
+
+    dealer_card_frame.destroy()
+    dealer_card_frame = tkinter.Frame(card_frame, background="green")
+    dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
+
+    player_card_frame.destroy()
+    player_card_frame = tkinter.Frame(card_frame, background="green")
+    player_card_frame.grid(row=2, column=1, sticky="ew", rowspan=2)
+
+    result_text.set("")
+
+    # load cards
+    cards = []
+    load_images(cards)
+    # print(cards)
+    # Create a new deck cards and shuffle them
+
+    deck = list(cards)  # Creating a new separate list
+    random.shuffle(deck)
+
+    # Create a lists to store the dealer's and player's hands
+    dealer_hand = []
+    player_hand = []
+
+    deal_player()
+    dealer_hand.append(deal_card(dealer_card_frame))
+    dealer_score_label.set(score_hand(dealer_hand))
+    deal_player()
+
+
 mainWindow = tkinter.Tk()
 
 # Set up the screen and frames
@@ -111,6 +149,7 @@ mainWindow.geometry("640x480")
 mainWindow.configure(background='green')
 
 result_text = tkinter.StringVar()
+
 result = tkinter.Label(mainWindow, textvariable=result_text)
 result.grid(row=0, column=0, columnspan=3)
 
@@ -154,22 +193,28 @@ player_button = tkinter.Button(button_frame, text="Player",
                                command=deal_player)
 player_button.grid(row=0, column=1)
 
-# load cards
-cards = []
-load_images(cards)
-# print(cards)
-# Create a new deck cards and shuffle them
+new_game_button = tkinter.Button(button_frame, text="New Game",
+                                 command=start_new_game)
+new_game_button.grid(row=0, column=2)
 
-deck = list(cards)  # Creating a new separate list
-random.shuffle(deck)
+start_new_game()
 
-# Create a lists to store the dealer's and player's hands
-dealer_hand = []
-player_hand = []
-
-deal_player()
-dealer_hand.append(deal_card(dealer_card_frame))
-dealer_score_label.set(score_hand(dealer_hand))
-deal_player()
+# # load cards
+# cards = []
+# load_images(cards)
+# # print(cards)
+# # Create a new deck cards and shuffle them
+#
+# deck = list(cards)  # Creating a new separate list
+# random.shuffle(deck)
+#
+# # Create a lists to store the dealer's and player's hands
+# dealer_hand = []
+# player_hand = []
+#
+# deal_player()
+# dealer_hand.append(deal_card(dealer_card_frame))
+# dealer_score_label.set(score_hand(dealer_hand))
+# deal_player()
 
 mainWindow.mainloop()
